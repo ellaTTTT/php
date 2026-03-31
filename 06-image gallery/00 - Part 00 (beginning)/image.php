1e08b45955a9e2c32fe3cd7f0f2a9b293fcf240b
ella2eliot <a038038456123@gmail.com>
2026-03-31 13:56:18 +0800

    Complete implementation of gallery.php.

--- 06-image gallery/00 - Part 00 (beginning)/image.php
+++ 06-image gallery/00 - Part 00 (beginning)/image.php
@@ -1,9 +1,22 @@
 <?php
 include './inc/functions.inc.php';
 include './inc/images.inc.php';
-
+$image = $_GET['image'];
 ?>
 <?php include './views/header.php'; ?>
 
+<?php if($image && !empty($imageTitles[$image])): ?>
+    <h3><?php echo e($imageTitles[$image]); ?></h3>
+    <img src="./images/<?php echo rawurldecode($image); ?>" />
+    <?php echo str_replace("\n", "<br />", e($imageDescriptions[$image])); ?>
+<?php else: ?>
+    <div>This image couldn't be found.</div>
+<?php endif; ?>
+</br>
+</br>
+<a href="gallery.php">Back to gallery</a>
+<br />
+<br />
+<br />
 
 <?php include './views/footer.php'; ?>
